c7dfeddbd3ad9b94bbfb3a3dcfa27e9f965bb192
Adam Kramar <adam@Adams-MacBook-Air.local>
2020-07-16 21:04:16 +0200

    initial commit

---
+++ TwitterCloneAdamKramar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitterCloneAdamKramar
+//
+//  Created by Adam Kramar on 16/07/2020.
+//  Copyright © 2020 Adam Kramar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
